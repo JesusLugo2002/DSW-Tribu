60d530dbac64e2b53f1661c0265910e6066cf21c
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-07 01:41:40 +0000

    Se hacen cambios

--- users/views.py
+++ users/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def user_list(request):
-    pass
+    users = User.objects.all().order_by('username')
+    return render(request, 'users/list.html', dict(users=users))
 
 
 def user_detail(request, username):
     other_user = User.objects.get(username=username)
-    return render(request, 'users/detail.html', dict(other_user=other_user))
+    return render(request, 'users/user/detail.html', dict(other_user=other_user))
 
 
 def user_echos(request, user_pk):
