143bb5e823584fdc9a55e4d4b1c1df02137a5c7f
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-10 21:42:43 +0000

    Se añade formulario de edicion de usuarios; no funciona la subida de avatares

---
+++ users/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from .models import Profile
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+from django.http import HttpResponseForbidden, HttpResponse
+
+from .models import Profile
+from .forms import EditProfileForm
 
 
 @login_required
@@ -34,4 +38,13 @@ def my_user(request):
 
 @login_required
 def user_edit(request, username):
-    pass
+    if request.user.username != username:
+        return HttpResponseForbidden("You can not edit another user profile!")
+    user_profile = Profile.objects.get(user=request.user)
+    form = EditProfileForm(request.POST or None, instance=user_profile)
+    if form.is_valid():
+        profile = form.save(commit=False)
+        profile.user = request.user
+        profile.save()
+        return redirect('users:my-user')
+    return render(request, "users/user/edit.html", dict(form=form))
