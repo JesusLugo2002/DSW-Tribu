e77b7cb553a23eb4ea1e3fa918dba355d6a1640b
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-11 09:50:04 +0000

    Se corrige codigo innecesario del modelo Profile

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.db import models
 
-
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile'
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
     )
     avatar = models.ImageField(
-        blank=True, null=True, upload_to='avatars', default='avatars/noavatar.png'
+        blank=True, 
+        null=True, 
+        upload_to='avatars', 
+        default='avatars/noavatar.png'
     )
     bio = models.TextField(blank=True)
