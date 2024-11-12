8860f6af9092b82f9eb032e7cc3811a32e1bb08a
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-12 11:53:36 +0000

    Se refactoriza vistas de waves

--- waves/views.py
+++ waves/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from shared.utils import assert_owner_of
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 
+from shared.utils import assert_owner_of
 from .forms import EditWaveForm
 from .models import Wave
 
 @login_required
 @assert_owner_of('wave')
-def wave_edit(request, wave_pk):
+def wave_edit(request: HttpRequest, wave_pk: int) -> HttpResponse | HttpResponseForbidden:
     wave = Wave.objects.get(pk=wave_pk)
     form = EditWaveForm(request.POST or None, instance=wave)
     if form.is_valid():
         wave = form.save()
-        return redirect('echos:echo-list')
+        return redirect(wave.echo)
     return render(request, 'waves/edit.html', dict(form=form))
 
 @login_required
 @assert_owner_of('wave')
-def wave_delete(request, wave_pk):
+def wave_delete(request: HttpRequest, wave_pk: int) -> HttpResponse | HttpResponseForbidden:
     wave = Wave.objects.get(pk=wave_pk)
+    echo = wave.echo
     wave.delete()
-    return redirect('echos:echo-list')
+    return redirect(echo)
